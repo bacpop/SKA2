bbca7bb8b3fc3bb9e5573760003106e32870f828
Johanna <j.vwachsmann@web.de>
2022-12-14 10:17:57 +0000

    add src

---
+++ src/api.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Johanna Helene von Wachsmann on 16/08/2022.
+//
+/*
+ * api.cpp
+ * Main functions for running sketches
+ *
+ */
+#include "api.hpp"
+
+#pragma omp critical
+{
---
+++ src/api.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Johanna Helene von Wachsmann on 16/08/2022.
+//
+
+#ifndef SKA2_API_H
+#define SKA2_API_H
+
+
+class api {
+
+};
+
+
+#endif //SKA2_API_H
